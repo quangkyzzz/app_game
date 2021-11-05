05398a13803c09072dc20a9d8214b835043a9468
quangkyzzz <57524232+quangkyzzz@users.noreply.github.com>
2021-11-05 14:40:32 +0700

    as

--- app/src/main/java/com/example/find_same_picture_game/PlayingActivity.java
+++ app/src/main/java/com/example/find_same_picture_game/PlayingActivity.java
@@ -3,14 +3,27 @@ package com.example.find_same_picture_game;
 import android.os.Bundle;
 import android.text.Layout;
 import android.util.DisplayMetrics;
+import android.util.TimeUtils;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PlayingActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+public class PlayingActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public  ArrayList<Integer> mImageList = new ArrayList<Integer>();
+    public  ArrayList<Integer> mButtonList = new ArrayList<Integer>();
+    public  int count;
+    public  ArrayList<Integer> checkOpen = new ArrayList<Integer>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
@@ -55,5 +68,118 @@ public class PlayingActivity extends AppCompatActivity {
 
 
 
+        mImageList.add(R.drawable.ic_bloom);
+        mImageList.add(R.drawable.ic_butterfree);
+        mImageList.add(R.drawable.ic_clefairy);
+        mImageList.add(R.drawable.ic_cyndaquil);
+        mImageList.add(R.drawable.ic_eevee);
+        mImageList.add(R.drawable.ic_jiggly);
+        mImageList.add(R.drawable.ic_pikachu);
+        mImageList.add(R.drawable.ic_spinda);
+        mImageList.add(R.drawable.ic_whiscash);
+        mImageList.add(R.drawable.ic_wooper);
+        mImageList.add(R.drawable.ic_bloom);
+        mImageList.add(R.drawable.ic_butterfree);
+        mImageList.add(R.drawable.ic_clefairy);
+        mImageList.add(R.drawable.ic_cyndaquil);
+        mImageList.add(R.drawable.ic_eevee);
+        mImageList.add(R.drawable.ic_jiggly);
+        mImageList.add(R.drawable.ic_pikachu);
+        mImageList.add(R.drawable.ic_spinda);
+        mImageList.add(R.drawable.ic_whiscash);
+        mImageList.add(R.drawable.ic_wooper);
+
+        mButtonList.add(R.id.button11);
+        mButtonList.add(R.id.button12);
+        mButtonList.add(R.id.button13);
+        mButtonList.add(R.id.button14);
+        mButtonList.add(R.id.button21);
+        mButtonList.add(R.id.button22);
+        mButtonList.add(R.id.button23);
+        mButtonList.add(R.id.button24);
+        mButtonList.add(R.id.button31);
+        mButtonList.add(R.id.button32);
+        mButtonList.add(R.id.button33);
+        mButtonList.add(R.id.button34);
+        mButtonList.add(R.id.button41);
+        mButtonList.add(R.id.button42);
+        mButtonList.add(R.id.button43);
+        mButtonList.add(R.id.button44);
+        mButtonList.add(R.id.button51);
+        mButtonList.add(R.id.button52);
+        mButtonList.add(R.id.button53);
+        mButtonList.add(R.id.button54);
+
+        count=0;
+        for (int i=0; i<20; i++){
+            checkOpen.add(0);
+        }
+
+
+    }
+
+    public Boolean checkScored(int poss){
+        for (int i=0; i<20; i++){
+            if (checkOpen.get(i) == 1 ){
+                if ( mImageList.get(i).equals(mImageList.get(poss)) ){
+                    ImageButton mButton = (ImageButton) findViewById(mButtonList.get(i));
+                    mButton.setVisibility(View.INVISIBLE);
+                    mButton.setClickable(false);
+
+                    mButton = (ImageButton) findViewById(mButtonList.get(poss));
+                    mButton.setVisibility(View.INVISIBLE);
+                    mButton.setClickable(false);
+                    checkOpen.set(i,0);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void onClick(View v) {
+        int poss = mButtonList.indexOf(v.getId());
+        ImageButton mButton;
+
+
+        if (checkOpen.get(poss) == 1) {
+            mButton = (ImageButton) findViewById(mButtonList.get(poss));
+        }
+        else if (count < 2) {
+            mButton = (ImageButton) findViewById(mButtonList.get(poss));
+            mButton.setBackgroundResource(mImageList.get(poss));
+            if (count == 0) {
+                count++;
+                checkOpen.set(poss, 1);
+            }
+            if (count == 1){
+                if (!checkScored(poss)){
+                    count++;
+                    checkOpen.set(poss,1);
+                }
+                else{
+                    count=0;
+                }
+
+            }
+
+        }
+        else {
+            for (int i=0; i<20; i++){
+                if (checkOpen.get(i) == 1){
+                    mButton = (ImageButton) findViewById(mButtonList.get(i));
+                    mButton.setBackgroundResource(R.drawable.question_button_state_list);
+                    checkOpen.set(i,0);
+                    count--;
+                }
+            }
+
+            mButton = (ImageButton) findViewById(mButtonList.get(poss));
+            mButton.setBackgroundResource(mImageList.get(poss));
+            checkOpen.set(poss,1);
+            count++;
+
+        }
     }
 }
